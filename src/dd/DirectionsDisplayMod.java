8ed4a7b98475f0dea8cc627972704b020c1c94d1
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2024-06-07 19:45:13 +0300

    Update DirectionsDisplayMod.java

--- src/dd/DirectionsDisplayMod.java
+++ src/dd/DirectionsDisplayMod.java
@@ -1,11 +1,28 @@
 package dd;
 
+import arc.*;
+import arc.util.*;
+import mindustry.game.EventType.*;
 import mindustry.mod.*;
-import dd.overlays.*;
+import mindustry.ui.dialogs.*;
 
 public class DirectionsDisplayMod extends Mod {
-    @Override
-    public void init() {
-        new DirectionOverlay().load();
+    
+    public DirectionsDisplayMod() {
+        Log.info("Loaded DirectionsDisplayMod constructor.");
+        
+        Events.on(ClientLoadEvent.class, e -> {
+            Time.runTask(10f, () -> {
+                Core.app.post(() -> {
+                    // Show an info dialog with the localized message
+                    Vars.ui.showInfo(Core.bundle.get("mod.startup.message"));
+                });
+            });
+        });
     }
-}
+
+    @Override
+    public void loadContent() {
+        Log.info("Loading some example content.");
+    }
+    }
