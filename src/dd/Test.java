5eb60a0317a572d1d9dcb619ac75d3bbc262e798
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2024-06-07 18:50:10 +0300

    Update Test.java

--- src/dd/Test.java
+++ src/dd/Test.java
@@ -17,18 +17,7 @@ public class directionsdisplay extends Mod{
 
         //listen for game load event
         Events.on(ClientLoadEvent.class, e -> {
-            //show dialog upon startup
-            Time.runTask(10f, () -> {
-                BaseDialog dialog = new BaseDialog("frog");
-                dialog.cont.add("behold").row();
-                //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
-                dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
-                dialog.cont.button("I see", dialog::hide).size(100f, 50f);
-                dialog.show();
-            });
-        });
-    }
-
+                
     @Override
     public void loadContent(){
         Log.info("Loading some example content.");
