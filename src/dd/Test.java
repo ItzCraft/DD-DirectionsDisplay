2de6051140f85829770ec366ddb0ebb7a6d06122
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2024-06-07 19:21:19 +0300

    Update DirectionsDisplayMod.java

--- src/dd/Test.java
+++ src/dd/Test.java
@@ -1,26 +1,11 @@
 package dd;
 
-import arc.*;
-import arc.util.*;
-import mindustry.*;
-import mindustry.content.*;
-import mindustry.game.EventType.*;
-import mindustry.gen.*;
 import mindustry.mod.*;
-import mindustry.ui.dialogs;
-import mindustry.ui.Fonts;
+import dd.overlays.*;
 
-public class directionsdisplay extends Mod{
-
-    public test(){
-        Log.info("Loaded ExampleJavaMod constructor.");
-
-        //listen for game load event
-        Events.on(ClientLoadEvent.class, e -> {
-                
+public class DirectionsDisplayMod extends Mod {
     @Override
-    public void loadContent(){
-        Log.info("Loading some example content.");
+    public void init() {
+        new DirectionOverlay().load();
     }
-
 }
