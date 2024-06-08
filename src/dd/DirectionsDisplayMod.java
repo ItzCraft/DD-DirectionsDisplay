6fd0d90e8479eb155a934dcc887eacffc06087ac
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2024-06-08 08:41:27 +0300

    Update DirectionsDisplayMod.java

--- src/dd/DirectionsDisplayMod.java
+++ src/dd/DirectionsDisplayMod.java
@@ -1,22 +1,27 @@
 package dd;
 
-import arc.*;
-import arc.util.*;
-import mindustry.game.EventType.*;
-import mindustry.mod.*;
-import mindustry.ui.dialogs.*;
-
-import static mindustry.Vars.*;
+import arc.Core;
+import arc.util.Log;
+import arc.util.Time;
+import mindustry.Vars;
+import mindustry.game.EventType.ClientLoadEvent;
+import mindustry.mod.Mod;
+import mindustry.ui.dialogs.BaseDialog;
 
 public class DirectionsDisplayMod extends Mod {
-    
+
     public DirectionsDisplayMod() {
         Log.info("Loaded DirectionsDisplayMod constructor.");
         
         Events.on(ClientLoadEvent.class, e -> {
             Time.runTask(10f, () -> {
                 Core.app.post(() -> {
-                    Vars.ui.showInfo(Core.bundle.get("dd.startup.message"));
+                    BaseDialog dialog = new BaseDialog("info");
+                    dialog.cont.add(Core.bundle.get("dd.startup.message")).row();
+                    dialog.buttons.defaults().size(210f, 64f);
+                    dialog.setFillParent(true);
+                    dialog.buttons.addButton("OK", dialog::hide);
+                    dialog.show();
                 });
             });
         });
@@ -26,4 +31,4 @@ public class DirectionsDisplayMod extends Mod {
     public void loadContent() {
         Log.info("Loading some example content.");
     }
-    }
+}
