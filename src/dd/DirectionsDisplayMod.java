8df0446e479ea9c3a92080ab6cfb943398a70df7
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2024-06-08 08:52:05 +0300

    Update DirectionsDisplayMod.java

--- src/dd/DirectionsDisplayMod.java
+++ src/dd/DirectionsDisplayMod.java
@@ -1,26 +1,28 @@
 package dd;
 
-import arc.Core;
-import arc.util.Log;
-import arc.util.Time;
-import mindustry.Vars;
-import mindustry.game.EventType.ClientLoadEvent;
-import mindustry.mod.Mod;
-import mindustry.ui.dialogs.BaseDialog;
+import arc.*;
+import arc.util.*;
+import mindustry.*;
+import mindustry.game.EventType.*;
+import mindustry.mod.*;
+import mindustry.ui.dialogs.*;
 
 public class DirectionsDisplayMod extends Mod {
 
     public DirectionsDisplayMod() {
         Log.info("Loaded DirectionsDisplayMod constructor.");
-        
+
+        // Add post-load initialization code here
         Events.on(ClientLoadEvent.class, e -> {
+            // Wait a bit before showing the message
             Time.runTask(10f, () -> {
                 Core.app.post(() -> {
+                    // Show an info dialog with the localized message
                     BaseDialog dialog = new BaseDialog("info");
                     dialog.cont.add(Core.bundle.get("dd.startup.message")).row();
                     dialog.buttons.defaults().size(210f, 64f);
                     dialog.setFillParent(true);
-                    dialog.buttons.addButton("OK", dialog::hide);
+                    dialog.cont.button("OK", dialog::hide);
                     dialog.show();
                 });
             });
@@ -31,4 +33,4 @@ public class DirectionsDisplayMod extends Mod {
     public void loadContent() {
         Log.info("Loading some example content.");
     }
-}
+            }
