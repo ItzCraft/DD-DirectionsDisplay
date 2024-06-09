5a762d9acf93e8ab011dc93afb9ec1fbacf40bfb
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2024-06-09 13:10:30 +0300

    Update DirectionOverlay.java

--- src/dd/DirectionOverlay.java
+++ src/dd/DirectionOverlay.java
@@ -15,11 +15,14 @@ import mindustry.world.blocks.distribution.*;
 
 public class DirectionOverlay {
     public void load() {
+        
+        Call.infoMessage("DirectionOverlay load method called");
+        
         Core.scene.add(new Table() {{
             update(() -> {
                 for (Building build : Groups.build) {
                     if (build.block instanceof Sorter) {
-                        Draw.z(Layer.overlayUI + 1);
+                        Draw.z(Layer.effect);
                         float x = build.x;
                         float y = build.y;
 
@@ -28,24 +31,31 @@ public class DirectionOverlay {
                         boolean left = isConveyorConnected(build, build.tileX() - 1, build.tileY());
                         boolean right = isConveyorConnected(build, build.tileX() + 1, build.tileY());
 
+                        Call.infoMessage("Checking sorter at (" + x + ", " + y + ")");
+                        Call.infoMessage("Connections - Top: " + top + ", Bottom: " + bottom + ", Left: " + left + ", Right: " + right);
+
                         if (top) {
+                            Call.infoMessage("Drawing text at the top of (" + x + ", " + y + ")");
                             Draw.color(Pal.gray);
-                            Drawf.label("Top", x, y + 8);
+                            Fonts.def.draw("Top", x, y + 8, Align.center);
                             Draw.reset();
                         }
                         if (bottom) {
+                            Call.infoMessage("Drawing text at the bottom of (" + x + ", " + y + ")");
                             Draw.color(Pal.gray);
-                            Drawf.label("Bottom", x, y - 8);
+                            Fonts.def.draw("Bottom", x, y - 8, Align.center);
                             Draw.reset();
                         }
                         if (left) {
+                            Call.infoMessage("Drawing text to the left of (" + x + ", " + y + ")");
                             Draw.color(Pal.gray);
-                            Drawf.label("Left", x - 8, y);
+                            Fonts.def.draw("Left", x - 8, y, Align.center);
                             Draw.reset();
                         }
                         if (right) {
+                            Call.infoMessage("Drawing text to the right of (" + x + ", " + y + ")");
                             Draw.color(Pal.gray);
-                            Drawf.label("Right", x + 8, y);
+                            Fonts.def.draw("Right", x + 8, y, Align.center);
                             Draw.reset();
                         }
                     }
@@ -58,8 +68,10 @@ public class DirectionOverlay {
         Tile tile = Vars.world.tile(tileX, tileY);
         if (tile != null) {
             Block block = tile.block();
+            Call.infoMessage("Tile found at (" + tileX + ", " + tileY + ") with block: " + block.name);
             return block instanceof Conveyor;
         } else {
+            Call.infoMessage("No tile found at (" + tileX + ", " + tileY + ")");
             return false;
         }
     }
